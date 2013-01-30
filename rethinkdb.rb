a38e63415add2013e0a4d684b9bbd5282db76654
Etienne Laurin <atnnn@rethinkdb.com>
2013-01-30 15:07:14 -0800

    add head to rethinkdb

--- rethinkdb.rb
+++ rethinkdb.rb
@@ -4,12 +4,14 @@ class Rethinkdb < Formula
   homepage 'http://www.rethinkdb.com/'
   url 'https://github.com/rethinkdb/rethinkdb/archive/v1.3.2.tar.gz'
   sha1 '8956087fb98f32fa5a320e696e28b239afe03ab4'
+  head 'git://github.com/rethinkdb/rethinkdb.git'
 
   depends_on 'v8'
   depends_on 'node' => :build
   depends_on 'protobuf' => :build
   depends_on 'less' => :node
   depends_on 'coffee-script' => :node
+  depends_on 'handlebars' => :node if build.head?
 
   def install
     system "cd src; make DEBUG=0 FORCEVERSION=1 RETHINKDB_VERSION=#{version} PREFIX=#{prefix} WEBRESDIR=#{share}/rethinkdb/web"
