0c153986bcbe8824ac119136f1ac40c145b1762b
Etienne Laurin <atnnn@rethinkdb.com>
2013-03-04 11:45:29 -0800

    private install for testing

--- rethinkdb.rb
+++ rethinkdb.rb
@@ -2,7 +2,9 @@ require 'formula'
 
 class Rethinkdb < Formula
   homepage 'http://www.rethinkdb.com/'
+  url 'http://newton:8800/rethinkdb-1.3.2-462.tgz'
   sha1 '629a6eed8e13eab8056389d123b203f115ce2e20'
+  version '1.3.2-462'
 
   depends_on 'v8'
   depends_on 'protobuf' => :build
