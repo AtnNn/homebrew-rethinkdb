e63fe2619d468f60b73b752df69bc76b22fd831b
Etienne Laurin <atnnn@rethinkdb.com>
2013-05-15 13:23:59 -0700

    test

--- rethinkdb-test.rb
+++ rethinkdb-test.rb
@@ -1,15 +1,15 @@
 require 'formula'
 
-class RethinkdbTest < Formula
+class Rethinkdb < Formula
   homepage 'http://www.rethinkdb.com/'
-  url 'http://newton:8800/dist/rethinkdb-1.3.2-462.tgz'
-  sha1 '629a6eed8e13eab8056389d123b203f115ce2e20'
-  version '1.3.2-462'
+  url 'http://dr-doom:8888/job/package-dist/55/artifact/rethinkdb-1.4.5-1374-ge23e46.tgz'
+  # sha1 '24f9b38b551e05ed560ca6ec85abd2276e29951e'
 
+  depends_on 'boost'
   depends_on 'v8'
-  depends_on 'protobuf' => :build
 
   def install
-    system "make PREFIX=#{prefix} install-osx"
+    system "./configure --prefix=#{prefix} --fetch protobuf --fetch protoc"
+    system "make install-osx"
   end
 end
